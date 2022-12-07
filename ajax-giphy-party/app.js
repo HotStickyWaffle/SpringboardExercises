c9df48f4525b3732d1c7946236fd85a9de63b5ee
Nick Salvemini <salvemini.nick@gmail.com>
2022-12-06 19:10:35 -0500

    continued working on giy exercise

--- ajax-giphy-party/app.js
+++ ajax-giphy-party/app.js
@@ -1,19 +1,91 @@
-console.log("Let's get this party started!");
-
-
-
 async function getGif(searchTerm) {
 
     const params = {
         q: searchTerm,
         api_key: 'LRHEtoKl6lsI8LBpqzu9uPgivxtNixon',
-        limit: 1,
+        limit: 50,
     }
-    const response = await axios.get('api.giphy.com/v1/gifs/search', params);
-    console.log(response)
+    const response = await axios.get('http://api.giphy.com/v1/gifs/search', { params });
+
+    let randomNum = Math.round(Math.random() * params.limit + 1);
+
+    return response.data.data[randomNum].url
+    // console.log(response.data.data[randomNum].url)
 }
 
-// async function getGif() {
-//     const response = await axios.get('http://api.giphy.com/v1/gifs/search?q=cheeseburgers&api_key=LRHEtoKl6lsI8LBpqzu9uPgivxtNixon&limit=5');
-//     console.log(response)
+const searchButton = document.querySelector('#searchButton')
+const gifDiv = document.querySelector('#gifDiv')
+
+
+searchButton.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    const searchTerm = document.querySelector('#searchTerm')
+    console.log(searchTerm.value)
+    const gifUrl = getGif(searchTerm.value)
+    console.log(gifUrl)
+    appendGif(gifUrl)
+
+})
+
+function appendGif(url) {
+    const newGif = document.createElement('img');
+    newGif.setAttribute('src', url);
+    gifDiv.append(newGif)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Gify {
+//     constructor(searchTerm) {
+//         this.searchTerm = searchTerm;
+//         this.api_key = 'LRHEtoKl6lsI8LBpqzu9uPgivxtNixon';
+//         this.limit = 1;
+
+//         this.addGif();
+//     }
+
+//     async getGif() {
+//         const params = {
+//             q: this.searchTerm,
+//             api_key: this.api_key,
+//             limit: this.limit,
+//         }
+
+//         const response = await axios.get('http://api.giphy.com/v1/gifs/search', { params });
+//         return response.data.data[0].url
+//     }
+
+//     addGif() {
+//         let self = this;
+
+//         let gifUrl = self.getGif()
+//         console.log(gifUrl)
+//     }
 // }
+
+// let newGifyRequest = new Gify('burgers');
+// newGifyRequest.getGif();
